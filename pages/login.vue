31ec393917a945705240262d4071681928a7dfb2
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-818.local>
2022-05-03 21:50:03 -0400

    Updated Styles

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-screen h-screen flex flex-col justify-center">
-        <section class="p-8 shadow w-min h-screen flex justify-center flex-col items-center">
+    <div class="w-screen h-screen flex flex-col justify-center items-center md:items-start">
+        <section class="p-8 md:shadow w-min h-screen flex justify-center flex-col items-center">
             <LoginForm />
         </section>
     </div>
